efb6e84f61e6a808d900bc5c3b3e7c6238bee035
Shawket Ablikim <shawkat101@hotmail.com>
2020-11-25 12:15:32 +1100

    GettingMoreInfoAboutResultObject class

---
+++ src/test/java/day2/GettingMoreInfoAboutResultObject.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.sql.*;
+
+public class GettingMoreInfoAboutResultObject {
+
+    public static void main(String[] args) throws SQLException {
+
+        // we want to create a statement object that generate
+        // ResultSet that can move forward and backward anytime
+        String connectionStr = "jdbc:oracle:thin:@54.162.198.60:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        // this way of creating statement will give you ability to generate
+        // ResultSet that can move forward and backward anytime
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs   =   stmt.executeQuery("SELECT * FROM JOBS") ;
+
+
+
+
+
+    }
+}
