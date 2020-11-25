8ae220d262d3fddd2f1483ed874ed88735264763
Shawket Ablikim <shawkat101@hotmail.com>
2020-11-25 11:49:23 +1100

    day2 add review class

--- src/test/java/day1/TestConnection.java
+++ src/test/java/day1/TestConnection.java
@@ -10,26 +10,34 @@ public class TestConnection {
         String username = "hr";
         String password = "hr";
 
-        Connection conn = DriverManager.getConnection(connectionStr, username, password);
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+
         Statement stmnt = conn.createStatement();
 
-        ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
-
+        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM REGIONS") ;
+        // this will move the cursor from beforefirst location to first row
         rs.next(); // currently we are at the very first row
+        // rs.getString(column index) can be used to read the data of that column in current row
+        System.out.println("first column value using index: --> " +  rs.getString(1)   );
+        System.out.println("second column value using index: --> " +  rs.getString(2)   );
 
-        System.out.println("first column value using index: --> " + rs.getString(1));
+        //  rs.getString(column LABEL)
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID") );
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME") );
 
-        System.out.println("first column value using index: --> " + rs.getString(2));
+        rs.next() ; // this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID") );
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME") );
 
-        System.out.println("Region_ID at this row is : " +rs.getString("REGION_ID"));
 
-        System.out.println("Region_Name at this row is : " +rs.getString("REGION_NAME"));
+        rs.next() ; // this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID") );
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME") );
 
-        rs.next();
 
-        System.out.println("Region_ID at this row is : " +rs.getString("REGION_ID"));
-
-        System.out.println("Region_Name at this row is : " +rs.getString("REGION_NAME"));
+        rs.next() ; // this will move us to next row so we can read next row
+        System.out.println("Region_ID at this row is : " + rs.getString("REGION_ID") );
+        System.out.println("Region_Name at this row is : " + rs.getString("REGION_NAME") );
 
 
 
---
+++ src/test/java/day2/Review.java
@@ -0,0 +1,61 @@
+package day2;
+
+import java.sql.*;
+
+public class Review {
+
+    public static void main(String[] args) throws SQLException {
+
+        // we want to create a statement object that generate
+        // ResultSet that can move forward and backward anytime
+        String connectionStr = "jdbc:oracle:thin:@54.162.198.60:1521:XE";
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
+        // this way of creating statement will give you ability to generate
+        // ResultSet that can move forward and backward anytime
+        Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        ResultSet rs   =   stmt.executeQuery("SELECT * FROM JOBS") ;
+
+        rs.next();
+        System.out.println("First column value in Jobs: " + rs.getString(1));
+        System.out.println("Second column value in Jobs: " + rs.getString(2));
+
+        rs.absolute(7);
+        System.out.println("First column value in Jobs in row 7: " + rs.getString(1));
+        System.out.println("Second column value in Jobs in row 7: " + rs.getString(2));
+
+        rs.last();
+        System.out.println("First column value in Jobs in last row : " + rs.getString(1));
+        System.out.println("Second column value in Jobs in last row: " + rs.getString(2));
+
+        rs.previous();
+        System.out.println("First column value in Jobs in 2nd row from last row : " + rs.getString(1));
+        System.out.println("Second column value in Jobs in 2nd row from last row: " + rs.getString(2));
+
+        System.out.println("-----loop from top to bottom print JOB_ID-----");
+
+        rs.beforeFirst();
+
+        while(rs.next()){
+            System.out.println("Loop First column : " + rs.getString("JOB_ID"));
+        }
+
+        System.out.println("-----loop from last row till first row get MIn_SALARY column as number-----");
+
+        rs.afterLast();
+
+        while(rs.previous()){
+            System.out.println("MIN SALARY COLUMN AS NUMBER: $" + rs.getDouble("MIN_SALARY"));
+        }
+
+
+
+
+
+
+
+    }
+}
